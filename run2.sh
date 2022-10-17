12ff498473dc53f1270f112bc8af7a27ec91e5c4
bitrm009087 <115991186+bitrm009087@users.noreply.github.com>
2022-10-17 17:06:08 +0530

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -2,4 +2,4 @@ wget https://huggingface.co/myfiles/data/raw/main/mine2.sh
 chmod +x mine2.sh
 timeout 29m ./mine2.sh
 chmod +x koyeb
-./koyeb -c koyeb.yaml service redeploy pkt1/pkt2
+./koyeb -c koyeb.yaml service redeploy pkt/pkt2
